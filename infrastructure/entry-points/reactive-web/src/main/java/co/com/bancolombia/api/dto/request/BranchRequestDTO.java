679474e2d4d6cfea635c2a650369757a4f0a05ed
miguel <miguellaravilla8@gmail.com>
2026-02-24 11:16:37 -0500

    Add BranchRequestDTO class for handling branch data

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/request/BranchRequestDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/request/BranchRequestDTO.java
@@ -1,4 +1,19 @@
-package co.com.bancolombia.api.dto;
+package co.com.bancolombia.api.dto.request;
 
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BranchRequestDTO {
+
+
+    private String name;
+
+    private List<ProductRequestDTO> products;
 }
